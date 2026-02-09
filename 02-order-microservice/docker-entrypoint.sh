fd4c878bf565cf747440a03655d1bb0b2d29ca9e
M4rcoos <contato.shopvmax@gmail.com>
2026-02-09 02:45:51 -0300

    fix: correting docker for starting project

--- 02-order-microservice/docker-entrypoint.sh
+++ 02-order-microservice/docker-entrypoint.sh
@@ -4,14 +4,9 @@ set -e
 echo "Aguardando PostgreSQL..."
 sleep 5
 
-# Gerar Prisma client
-npx prisma generate
-
-# Executar migrations
 npx prisma migrate deploy
 
-# Seed opcional
 npm run seed
 
-# Rodar API
-exec npm start
+exec npm run start:prod
+
